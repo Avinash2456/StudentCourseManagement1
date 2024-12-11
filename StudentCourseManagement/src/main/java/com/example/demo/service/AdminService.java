7aa96d354987fa50f6bb73557bb9e7e18008c8fd
Avinash2456 <144128469+Avinash2456@users.noreply.github.com>
2024-12-11 22:17:59 +0530

    Update AdminService.java

--- StudentCourseManagement/src/main/java/com/example/demo/service/AdminService.java
+++ StudentCourseManagement/src/main/java/com/example/demo/service/AdminService.java
@@ -2,11 +2,13 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Admin;
+import com.example.demo.model.Contact;
 import com.example.demo.model.Course;
 import com.example.demo.model.Student;
-
+@Service
 public interface AdminService {
 	
 	public List<Student> viewAllStudents();
@@ -15,5 +17,13 @@ public interface AdminService {
 	public long coursecount();
 	void addCourse(Course course);
 	public List<Course> viewAllCourses();
+	public String SaveContact(Contact contact);
+	public String deleteStudent(int id);
+	public Student displayStudentById(int id); 
+	public Student updateStudent(Student student);
+	public String deleteCourse(Long id);
+	public Course displayCourseById(Long id);
+	public Course updateCourse(Course course);
 
+	
 }
